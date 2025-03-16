2b2ac3daa1a098c720370b504cb769f1c6c4b8b9
Manasbhunya23 <manasbhunya23@gmail.com>
2025-03-16 13:54:31 +0530

    ENV changes and neon attached

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a-pt*q60k6u&*2kob7^))=6#8p1^i+fe)-=x%y0%of)@75bvp)'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DEBUG")).lower() == "true"
+#& DEBUG = str(os.environ.get("DEBUG")).lower() == "true"
+DEBUG = config("DJANGO_DEBUG", cast = bool)
 
-# print("DEBUG: ",DEBUG)
+print("DEBUG: ",DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app" #~ https//saas.prod.railway.app
@@ -91,6 +92,18 @@ DATABASES = {
     }
 }
 
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default = 30)
+DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_health_checks=True,
+            conn_max_age=CONN_MAX_AGE,     
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
